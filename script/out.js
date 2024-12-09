b13d1e52f5531388c5ee1ffbc08e705136b80156
Lord9s <nkunajunior399@gmail.com>
2024-12-09 13:14:08 +0200

    Update out.js

--- script/out.js
+++ script/out.js
@@ -3,7 +3,7 @@ module.exports.config = {
   version: "1.0.0",
   role: 2,
   hasPrefix: true,
-  credits: "Developer",
+  credits: "𝗝𝗮𝘆 𝗹𝗼𝗿𝗱",
   description: "Bot leaves the thread",
   usages: "out",
   cooldowns: 10,
@@ -13,7 +13,7 @@ module.exports.config = {
 module.exports.run = async function({ api, event, args }) {
   try { 
   if (!args[0]) return api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
-  if (!isNaN(args[0])) return api.removeUserFromGroup(api.getCurrentUserID(), args.join(" "));
+  if (!isNaN(args[0])) return api.removeUserFromGroup(api.getCurrentUserID(), args.join("𝗢𝗛𝗛 𝗡𝗢 𝗠𝗬 𝗗𝗘𝗩𝗘𝗟𝗢𝗣𝗘𝗥 𝗔𝗦𝗞𝗘𝗗 𝗠𝗘 𝗧𝗢 𝗟𝗘𝗔𝗩𝗘 𝗧𝗛𝗘 𝗚𝗥𝗢𝗨𝗣 𝗦𝗢 𝗚𝗢𝗢𝗗 𝗕𝗬𝗘 𝗘𝗩𝗘𝗥𝗬𝗢𝗡𝗘 ⊂(・﹏・⊂)."));
     } catch (error) {
       api.sendMessage(error.message, event.threadID, event.messageID);
     }
