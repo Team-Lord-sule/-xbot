32b48e6e1f3894e99b08b71238f458516a03ee90
Lord9s <nkunajunior399@gmail.com>
2024-12-09 13:38:45 +0200

    Update trans.js

--- script/trans.js
+++ script/trans.js
@@ -5,7 +5,7 @@ module.exports.config = {
   hasPrefix: true,
   description: "Text translation",
   usages: "trans [tl, en] [promt]",
-  credits: "Developer",
+  credits: "jay lord",
   cooldowns: 5,
 };
 module.exports.run = async ({
