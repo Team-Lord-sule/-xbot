7b947884cf32d631b7aef47adf4c311968b98c02
Lord9s <nkunajunior399@gmail.com>
2025-01-29 18:49:23 +0200

    Update acp.js

--- script/acp.js
+++ script/acp.js
@@ -1,7 +1,7 @@
 module.exports.config = {
   name: "acp",
   version: "1.0.0",
-  hasPermssion: 2,
+ hasPrefix: false,
   credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
   description: "Make friends via Facebook id",
   commandCategory: "bot id",
